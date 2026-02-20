c0f4849392d0ab1bbe7cf17473d4c69b643767a0
Altakie <artemlavrov@brandeis.edu>
2026-02-20 17:05:50 -0500

    Basic fps count

--- level_loader.c
+++ level_loader.c
@@ -1,362 +1,372 @@
+#include <bits/time.h>
 #include <bits/types/struct_timeval.h>
+#include <iso646.h>
+#include <ncurses.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <sys/ioctl.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <sys/select.h>
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-#include <stdbool.h>
-
 
 #define LEVEL_FILE "level1"
 #define PLAYER_SPRITE "default.sprite"
-
 
 /* TODO
  * - Make renderer separate structure that is passed into functions
  * - Make physics engine
  * - Add collisions
  * - Refactor level loading
-*  - Add animations (swapping between sprites when velocity is 0)
-*  - Refactor input handling and movement system
-*/
-// Goal of this file is to create a simple level 
+ *  - Add animations (swapping between sprites when velocity is 0)
+ *  - Refactor input handling and movement system
+ */
+// Goal of this file is to create a simple level
 
 // Maybe make the level dynamically generated based off another file
 // Maybe make a level editor
 
+typedef struct {
+  int x;
+  int y;
+} Vector2;
 
 typedef struct {
-	int x;
-	int y;
-} vector2;
+  char **texture;
+  uint lengthpx;
+  uint heightpx;
+} Sprite;
 
 typedef struct {
-	char ** texture;
-	uint lengthpx;
-	uint heightpx;
-} sprite;
+  Sprite texture;
+
+} Level;
 
 typedef struct {
-	sprite texture;
-
-} level;
-
-typedef struct {
-	vector2 position;
-	vector2 dimensions;
-} hitbox;
+  Vector2 position;
+  Vector2 dimensions;
+} Hitbox;
 
 typedef struct {
-	sprite sprite;
-	vector2 position;
-	vector2 velocity;
-	vector2 acceleration;
-} player;
-
+  Sprite sprite;
+  Vector2 position;
+  Vector2 velocity;
+  Vector2 acceleration;
+} Player;
 
 struct winsize getWindowSize();
 
-int load_level(char * file_name, level *level_data);
-int print_level(level *level_data, struct winsize ws);
-int render_pixels(vector2 pos, char * pixels, struct winsize ws);
-void move_cursor_to_pos(vector2 pos, struct winsize ws);
+int load_level(char *file_name, Level *level_data);
+int print_level(Level *level_data, struct winsize ws);
+int render_pixels(Vector2 pos, char *pixels);
+void move_cursor_to_pos(Vector2 pos, struct winsize ws);
 void disable_canonical_mode();
 int check_input();
-int render_sprite(sprite *sprite, vector2 pos, struct winsize ws);
-int init_player(player *p, struct winsize ws, level level_data);
-int load_sprite_texture(sprite *sprite, char *filename);
+int render_sprite(Sprite *sprite, Vector2 pos);
+int init_player(Player *p, Level level_data);
+int load_sprite_texture(Sprite *sprite, char *filename);
 int max(int *arr, int len);
 
+int main(int argc, char **argv) {
+  // system("stty -echo");
+  // disable_canonical_mode();
+  initscr();
+  noecho();
+  cbreak();
+  curs_set(0);
+  // Should use another thread for input handling
 
-int main(int argc, char ** argv) {
+  // struct winsize ws = getWindowSize();
+  // struct winsize ws;
+  // if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) != 0) {
+  //   return 1;
+  // }
 
-	system("stty -echo");
-	disable_canonical_mode();
-	// Should use another thread for input handling
+  // int wlength = ws.ws_col;
+  // int wheight = ws.ws_row;
 
+  Level level_data = {0};
 
-	// struct winsize ws = getWindowSize();
-	struct winsize ws;
-	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) != 0) {
-		return 1;
-	}
+  load_level(LEVEL_FILE, &level_data);
 
-	int wlength = ws.ws_col;
-	int wheight = ws.ws_row;
+  // Recursively initializes the player on the stack
+  Player player1 = {0};
+  init_player(&player1, level_data);
 
+  int frame_count = 0;
+  struct timespec before;
+  clock_gettime(CLOCK_MONOTONIC, &before);
 
-	level level_data = {0};
+  while (true) {
 
-	load_level(LEVEL_FILE, &level_data);
+    // print_level(&level_data, ws);
 
-	// Recursively initializes the player on the stack
-	player player1 = {0};
-	init_player(&player1, ws, level_data);
+    if (check_input() != 0) {
+      char input = getchar();
+      switch (input) {
+      case 'a':
+        player1.position.x -= 1;
+        break;
+      case 'd':
+        player1.position.x += 1;
+        break;
+      case 'w':
+        player1.position.y += 1;
+        break;
+      case 's':
+        player1.position.y -= 1;
+        break;
+      default:
+        break;
+      }
+    }
 
-	while (true) {
+    render_sprite(&player1.sprite, player1.position);
+    // fflush(stdout);
+    refresh();
+    // Reset Cursor to top left and clear screen
+    printw("\033[1;1H\r");
 
-		print_level(&level_data, ws);
+    frame_count++;
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    double duration = ((double)(now.tv_sec - before.tv_sec) +
+                       ((double)(now.tv_nsec - before.tv_nsec) / 1e9));
+    double fps = frame_count / duration;
+    printw("Fps: %f", fps);
 
-		if (check_input() != 0) {
-			char input = getchar();
-			switch (input) {
-				case 'a':
-					 player1.position.x -= 1;
-					break;
-				case 'd':
-					player1.position.x += 1;
-					break;
-				case 'w':
-					player1.position.y += 1;
-					break;
-				case 's':
-					player1.position.y -= 1;
-					break;
-				default:
-					break;
-			}
-		}
+    struct timespec sleeptime;
+    sleeptime.tv_sec = 0;
+    sleeptime.tv_nsec = 5 * 1000000;
+    nanosleep(&sleeptime, &sleeptime);
+  }
 
-		render_sprite(&player1.sprite, player1.position, ws);
-		fflush(stdout);
-		printf("\033[1;1H\r");
+  // Move cursor up n lines to overwrite screen
+  // printw("\033[%dA\r", wheight);
+  //
+  //
+  // fflush(stdout);
+  // sleep(1);
 
-		struct timespec sleeptime;
-		sleeptime.tv_sec = 0;
-		sleeptime.tv_nsec = 10 * 1000000;
-		nanosleep(&sleeptime, &sleeptime);
-	}
-	
-	// Move cursor up n lines to overwrite screen
-	// printf("\033[%dA\r", wheight);
-	//
-	//
-	// fflush(stdout);
-	// sleep(1);
-
-	system("stty echo");
-
+  endwin();
+  // system("stty echo");
 }
 
+int init_player(Player *p, Level level_data) {
+  load_sprite_texture(&p->sprite, PLAYER_SPRITE);
 
-int init_player(player *p, struct winsize ws, level level_data) {
-	load_sprite_texture(&p->sprite, PLAYER_SPRITE);
+  // Set default position for player
+  Vector2 *pos = &p->position;
+  int x, y;
+  getmaxyx(stdscr, y, x);
+  pos->x = 10 + (-level_data.texture.lengthpx) / 2;
+  pos->y = 2;
 
-	// Set default position for player
-	vector2 *pos = &p->position;
-	pos->x = 10 + (ws.ws_col - level_data.texture.lengthpx) / 2;
-	pos->y = 2;
-
-	return 0;
+  return 0;
 }
 
 int max(int *arr, int len) {
-	if (len == 0) {
-		return 0; 
-	}
+  if (len == 0) {
+    return 0;
+  }
 
-	int max = arr[0];
-	for (int i = 1; i < len; i++) {
-		if (max < arr[i]) {
-			max = arr[i];
-		}
-	}
+  int max = arr[0];
+  for (int i = 1; i < len; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
 
-	return max;
-}
- 
-int load_sprite_texture(sprite *sprite, char *filename) {
-	FILE *sprite_file;
-	sprite_file = fopen(filename, "r");
-
-	if (sprite_file == NULL) {
-		printf("error opening sprite file");
-		return 1;
-	}
-
-	// Find size of file
-	if (fseek(sprite_file, 0L, SEEK_END) != 0) {
-		perror("Error seeking to EOF");
-		return 2;
-	}
-	long size = ftell(sprite_file);
-
-	// Reset to start of file
-	fseek(sprite_file, 0L, SEEK_SET);
-
-	char *file_buffer = malloc(size + 1);
-
-	if (fread(file_buffer, 1, size, sprite_file) < size) {
-		printf("error reading file\n");
-		return 3;
-	}
-	file_buffer[size] = '\0';
-
-	// Count lines and line lengths
-	int line_count = 0;
-	for (int i = 0; i < size; i++) {
-		char currchar = file_buffer[i];
-		if (currchar == '\n') {
-			line_count++;
-		}
-	}
-
-	int line_lengths[line_count];
-	int curr_line = 0;
-	int line_length = 0;
-	for (int i = 0; i < size; i++) {
-		char currchar = file_buffer[i];
-		if (currchar == '\n' || currchar == '\0') {
-			// Save length of this line
-			line_lengths[curr_line] = line_length;
-
-			// Reset
-			line_length = 0;
-			curr_line++;
-		} else {
-			line_length++;
-		}
-	}
-
-	// Only really want largest line length
-	int max_line_length = max(line_lengths, line_count);
-
-	// Format sprite into 2D array
-	char **texture = malloc(sizeof(char *) * line_count);
-	int pos = 0;
-	for (int i = 0; i < line_count; i++) {
-		char *line = malloc(sizeof(char) * (max_line_length + 1));
-		int curr_line_len = line_lengths[i];
-		// Fill up the line
-		for (int j = 0; j < curr_line_len; j++) {
-			char currchar = file_buffer[pos];
-			line[j] = currchar;
-			pos++;
-		}
-
-		for (int j = curr_line_len; j < max_line_length; j++) {
-			line[j] = ' ';
-		}
-
-		pos++; // To skip newline
-
-
-		line[max_line_length] = '\0';
-
-		texture[i] = line;
-	}
-
-
-	sprite->texture = texture;
-	sprite->heightpx = line_count;
-	sprite->lengthpx = max_line_length;
-
-
-	// Cleanup
-	free(file_buffer);
-	fclose(sprite_file);
-
-	return 0;
+  return max;
 }
 
- int load_level(char * file_name, level *level_data) {
-	load_sprite_texture(&level_data->texture, LEVEL_FILE);
-	return 0;
+int load_sprite_texture(Sprite *sprite, char *filename) {
+  FILE *sprite_file;
+  sprite_file = fopen(filename, "r");
+
+  if (sprite_file == NULL) {
+    printw("error opening sprite file");
+    return 1;
+  }
+
+  // Find size of file
+  if (fseek(sprite_file, 0L, SEEK_END) != 0) {
+    perror("Error seeking to EOF");
+    return 2;
+  }
+  long size = ftell(sprite_file);
+
+  // Reset to start of file
+  fseek(sprite_file, 0L, SEEK_SET);
+
+  char *file_buffer = malloc(size + 1);
+
+  if (fread(file_buffer, 1, size, sprite_file) < size) {
+    printw("error reading file\n");
+    return 3;
+  }
+  file_buffer[size] = '\0';
+
+  // Count lines and line lengths
+  int line_count = 0;
+  for (int i = 0; i < size; i++) {
+    char currchar = file_buffer[i];
+    if (currchar == '\n') {
+      line_count++;
+    }
+  }
+
+  int line_lengths[line_count];
+  int curr_line = 0;
+  int line_length = 0;
+  for (int i = 0; i < size; i++) {
+    char currchar = file_buffer[i];
+    if (currchar == '\n' || currchar == '\0') {
+      // Save length of this line
+      line_lengths[curr_line] = line_length;
+
+      // Reset
+      line_length = 0;
+      curr_line++;
+    } else {
+      line_length++;
+    }
+  }
+
+  // Only really want largest line length
+  int max_line_length = max(line_lengths, line_count);
+
+  // Format sprite into 2D array
+  char **texture = malloc(sizeof(char *) * line_count);
+  int pos = 0;
+  for (int i = 0; i < line_count; i++) {
+    char *line = malloc(sizeof(char) * (max_line_length + 1));
+    int curr_line_len = line_lengths[i];
+    // Fill up the line
+    for (int j = 0; j < curr_line_len; j++) {
+      char currchar = file_buffer[pos];
+      line[j] = currchar;
+      pos++;
+    }
+
+    for (int j = curr_line_len; j < max_line_length; j++) {
+      line[j] = ' ';
+    }
+
+    pos++; // To skip newline
+
+    line[max_line_length] = '\0';
+
+    texture[i] = line;
+  }
+
+  sprite->texture = texture;
+  sprite->heightpx = line_count;
+  sprite->lengthpx = max_line_length;
+
+  // Cleanup
+  free(file_buffer);
+  fclose(sprite_file);
+
+  return 0;
+}
+
+int load_level(char *file_name, Level *level_data) {
+  load_sprite_texture(&level_data->texture, LEVEL_FILE);
+  return 0;
 }
 
 int pad_left(int px) {
-	if (px < 0) {
-		return 1;
-	}
+  if (px < 0) {
+    return 1;
+  }
 
-	for (int i = 0; i < px; i++) {
-		printf(" ");
-	}
+  for (int i = 0; i < px; i++) {
+    printw(" ");
+  }
 
-	return 0;
+  return 0;
 }
 
 int pad_top_bottom(int px) {
-	if (px < 0) {
-		return 1;
-	}
-	for (int i = 0; i < px; i++) {
-		printf("\n");
-	}
+  if (px < 0) {
+    return 1;
+  }
+  for (int i = 0; i < px; i++) {
+    printw("\n");
+  }
 
-	return 0;
+  return 0;
 }
 
-int print_level(level *level_data, struct winsize ws) {
-	pad_top_bottom(ws.ws_row - level_data->texture.heightpx);
-	for (int i = 0; i < level_data->texture.heightpx; i++) {
-		char * row = level_data->texture.texture[i];
-		pad_left((ws.ws_col - level_data->texture.lengthpx)/2);
-		printf("%s", row);
-		if ((i + 1) != level_data->texture.heightpx) {
-			printf("\n");
-		}
-	}	
+int print_level(Level *level_data, struct winsize ws) {
+  pad_top_bottom(ws.ws_row - level_data->texture.heightpx);
+  for (int i = 0; i < level_data->texture.heightpx; i++) {
+    char *row = level_data->texture.texture[i];
+    pad_left((ws.ws_col - level_data->texture.lengthpx) / 2);
+    printw("%s", row);
+    if ((i + 1) != level_data->texture.heightpx) {
+      printw("\n");
+    }
+  }
 
-	return 0;
+  return 0;
 }
 
-int render_pixels(vector2 pos, char * pixels, struct winsize ws) {
-	move_cursor_to_pos(pos, ws);
-	printf("%s", pixels);
-
-	return 0;
+int render_pixels(Vector2 pos, char *pixels) {
+  // move_cursor_to_pos(pos, ws);
+  // printw("%s", pixels);
+  mvwprintw(stdscr, pos.y, pos.x, "%s", pixels);
+  return 0;
 }
 
-
-
-void move_cursor_to_pos(vector2 pos, struct winsize ws) {
-	// Convert vector2 into screen coordinates
-	// ANSI Escape codes think 1,1 is top left
-	// Want 0,0 to be bottom left of our map
-	int x = pos.x + 1;
-	int y = ws.ws_row - pos.y - 1;
-	printf("\033[%d;%dH", y, x);
+void move_cursor_to_pos(Vector2 pos, struct winsize ws) {
+  // Convert vector2 into screen coordinates
+  // ANSI Escape codes think 1,1 is top left
+  // Want 0,0 to be bottom left of our map
+  int x = pos.x + 1;
+  int y = ws.ws_row - pos.y - 1;
+  printw("\033[%d;%dH", y, x);
 }
 
 void disable_canonical_mode() {
-	struct termios attr;
-	tcgetattr(STDIN_FILENO, &attr);
+  struct termios attr;
+  tcgetattr(STDIN_FILENO, &attr);
 
-	attr.c_lflag &= ~(ICANON | ECHO);
+  attr.c_lflag &= ~(ICANON | ECHO);
 
-	tcsetattr(STDIN_FILENO, TCSANOW, &attr);
+  tcsetattr(STDIN_FILENO, TCSANOW, &attr);
 }
 
 int check_input() {
-	fd_set file_descriptor_set;
-	struct timeval timeout;
+  fd_set file_descriptor_set;
+  struct timeval timeout;
 
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 0;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
 
-	FD_ZERO(&file_descriptor_set);
-	FD_SET(STDIN_FILENO, &file_descriptor_set);
+  FD_ZERO(&file_descriptor_set);
+  FD_SET(STDIN_FILENO, &file_descriptor_set);
 
-	if (select(STDIN_FILENO + 1, &file_descriptor_set, NULL, NULL, &timeout) == -1) {
-		return 0;
-	}
+  if (select(STDIN_FILENO + 1, &file_descriptor_set, NULL, NULL, &timeout) ==
+      -1) {
+    return 0;
+  }
 
-	return FD_ISSET(STDIN_FILENO, &file_descriptor_set);
+  return FD_ISSET(STDIN_FILENO, &file_descriptor_set);
 }
 
-int render_sprite(sprite *sprite, vector2 pos, struct winsize ws) {
-	vector2 temppos;
-	temppos.x = pos.x;
-	temppos.y = pos.y;
-	char **texture = sprite->texture;
-	int heightpx = sprite->heightpx;
-	for (int i = heightpx - 1; i >= 0; i--) {
-		render_pixels(temppos, texture[i], ws);
-		temppos.y++;
-	}
+int render_sprite(Sprite *sprite, Vector2 pos) {
+  Vector2 temppos;
+  temppos.x = pos.x;
+  temppos.y = pos.y;
+  char **texture = sprite->texture;
+  int heightpx = sprite->heightpx;
+  for (int i = heightpx - 1; i >= 0; i--) {
+    render_pixels(temppos, texture[i]);
+    temppos.y++;
+  }
 
-	return 0;
+  return 0;
 }
